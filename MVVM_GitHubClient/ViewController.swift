b656967730e61d0d5daf30214b4baadb9d0d338b
Keisuke2162 <applabo.kei@gmail.com>
2020-05-11 11:17:28 +0900

    Initial Commit

---
+++ MVVM_GitHubClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_GitHubClient
+//
+//  Created by 植田圭祐 on 2020/05/11.
+//  Copyright © 2020 Keisuke Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
